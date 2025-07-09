01da9ee171c5ce8aa951f8cd13340250585c94ed
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-07-09 06:13:17 -1000

    fb

--- scripts/get_top_repos.sh
+++ scripts/get_top_repos.sh
@@ -3,18 +3,13 @@
 # Get top 10 repos by stars for nshkrdotcom using GitHub CLI
 # Output format: JSON with link and title
 
-echo '['
-
 # Get repos for user nshkrdotcom sorted by stars
 gh api users/nshkrdotcom/repos \
   --method GET \
   --field per_page="100" \
   --field sort="updated" \
-  --jq 'sort_by(.stargazers_count) | reverse | .[0:20] | to_entries | map({
-    "link": .value.html_url,
-    "title": .value.name,
-    "stars": .value.stargazers_count
-  }) | .[] | 
-  "  {\"link\": \"" + .link + "\", \"title\": \"" + .title + "\", \"stars\": " + (.stars | tostring) + "},"' | sed '$ s/,$//'
-
-echo ']'
+  --jq 'sort_by(.stargazers_count) | reverse | .[0:20] | map({
+    "link": .html_url,
+    "title": .name,
+    "stars": .stargazers_count
+  })'
